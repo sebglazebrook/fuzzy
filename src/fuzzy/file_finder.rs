491b5ef7a0bd2cd5998f2b86e4f4a23a31d8c6d8
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-08-28 20:21:20 +1000

    Move function to struct method

--- src/fuzzy/file_finder.rs
+++ src/fuzzy/file_finder.rs
@@ -44,20 +44,21 @@ impl FileFinder {
         let mut filepaths = vec![];
         for entry in fs::read_dir(dir).unwrap() {
             let entry = entry.unwrap();
-            filepaths.push(sanitize_file_path(entry.path().into_os_string().into_string().unwrap()));
+            filepaths.push(self.sanitize_file_path(entry.path().into_os_string().into_string().unwrap()));
             let attr = fs::metadata(entry.path()).unwrap();
             if attr.is_dir() {
                 // each one of these could be a new thread ??
                 let further = self.filepaths_in_directory(&entry.path().as_path());
                 for item in further.iter() {
-                    filepaths.push(sanitize_file_path(item.to_string()).clone());
+                    filepaths.push(self.sanitize_file_path(item.to_string()).clone());
                 }
             }
         }
         filepaths
     }
+
+    fn sanitize_file_path(&self, path: String) -> String {
+        path[1..].to_string()
+    }
 }
 
-fn sanitize_file_path(path: String) -> String {
-    path[1..].to_string()
-}
