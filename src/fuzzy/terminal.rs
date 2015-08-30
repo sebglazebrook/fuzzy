3638463638a5d54a12c2bd6af05fd8a8d409d60d
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-08-30 21:07:43 +1000

    Hacking together a way to allow the user to scroll through the results
    It's currently buggy.

--- src/fuzzy/terminal.rs
+++ src/fuzzy/terminal.rs
@@ -9,7 +9,8 @@ use std::thread;
 
 pub struct Terminal {
     pub rustbox: Arc<Mutex<RustBox>>,
-    pub results: Vec<String>,
+    results: Mutex<Vec<String>>,
+    hightlighted_result_row: Mutex<Vec<usize>>,
 }
 
 impl Terminal {
@@ -19,7 +20,7 @@ impl Terminal {
             Result::Ok(v) => Arc::new(Mutex::new(v)),
             Result::Err(e) => panic!("{}", e),
         };
-        Arc::new(Terminal {rustbox: rustbox, results: vec![] } )
+        Arc::new(Terminal {rustbox: rustbox, results: Mutex::new(vec![]), hightlighted_result_row: Mutex::new(vec![0]) } )
     }
 
     pub fn on_stdin<'a>(&self, search_phrase: Arc<Mutex<SearchPhrase>>) {
@@ -34,7 +35,7 @@ impl Terminal {
                         match key {
                             Some(Key::Ctrl('c')) => { break; }
                             Some(Key::Char(c)) => { 
-                                rustbox.print(character_index, 0, rustbox::RB_NORMAL, Color::White, Color::Black, &c.to_string());
+                                rustbox.print(character_index, 0, rustbox::RB_REVERSE, Color::White, Color::Black, &c.to_string());
                                 rustbox.present();
                                 character_index = character_index + 1;
 
@@ -65,6 +66,18 @@ impl Terminal {
                                     local_search_phrase.delete_last();
                                 });
                             }
+                            Some(Key::Ctrl('j')) => {
+                                self.hightlight_next_row(&rustbox);
+                            }
+                            Some(Key::Ctrl('k')) => {
+                                self.hightlight_previous_row(&rustbox);
+                            }
+                            Some(Key::Down) => {
+                                self.hightlight_next_row(&rustbox);
+                            }
+                            Some(Key::Up) => {
+                                self.hightlight_previous_row(&rustbox);
+                            }
                             Some(Key::Enter) => { done = true; }
                             _ => {  }
                         }
@@ -81,20 +94,22 @@ impl Terminal {
         self.clear_results();
         let rustbox = self.rustbox.clone();
         let rustbox = rustbox.lock().unwrap();
-//        rustbox.print(0, 1, rustbox::RB_NORMAL, Color::White, Color::Black, "Here are the matching results:");
         let mut starting_row = 0;
         for result in results.iter() {
             starting_row = starting_row + 1;
             rustbox.print(0, starting_row, rustbox::RB_NORMAL, Color::White, Color::Black, result);
         }
         rustbox.present();
+        let mut locked_results = self.results.lock().unwrap();
+        locked_results.clear();
+        locked_results.extend(results);
     }
 
     fn clear_results(&self) {
         let rustbox = self.rustbox.clone();
         let rustbox = rustbox.lock().unwrap();
         // clear all result rows
-        let mut empty_line = String::new();
+        let mut empty_line = String::new(); // TODO there must be a better way of doing this in rust
         for _ in 1..rustbox.width() {
             empty_line = empty_line.clone() + " ";
         }
@@ -102,4 +117,31 @@ impl Terminal {
             rustbox.print(0, row, rustbox::RB_NORMAL, Color::White, Color::Black, &empty_line);
         }
     }
+
+    fn hightlight_next_row(&self, rustbox: &RustBox) {
+        let mut hightlighted_result_row = self.hightlighted_result_row.lock().unwrap();
+        let results = self.results.lock().unwrap();
+        // unhighlight the current row
+        if hightlighted_result_row[0] > 0 {
+            rustbox.print(0, hightlighted_result_row[0], rustbox::RB_NORMAL, Color::White, Color::Black, &results[(hightlighted_result_row[0] - 1)]);
+        }
+        // highlight next row
+        hightlighted_result_row[0] = hightlighted_result_row[0] + 1;
+        rustbox.print(0, hightlighted_result_row[0], rustbox::RB_NORMAL, Color::Red, Color::Black, &results[(hightlighted_result_row[0] - 1)]);
+        rustbox.present();
+    }
+
+    fn hightlight_previous_row(&self, rustbox: &RustBox) {
+        let mut hightlighted_result_row = self.hightlighted_result_row.lock().unwrap();
+        let results = self.results.lock().unwrap();
+        // unhighlight the current row
+        if hightlighted_result_row[0] > 0 {
+            rustbox.print(0, hightlighted_result_row[0], rustbox::RB_NORMAL, Color::White, Color::Black, &results[(hightlighted_result_row[0] - 1)]);
+        }
+        // hightlight the previous row
+        hightlighted_result_row[0] = hightlighted_result_row[0] - 1;
+        rustbox.print(0, hightlighted_result_row[0], rustbox::RB_NORMAL, Color::Red, Color::Black, &results[(hightlighted_result_row[0] - 1)]);
+        rustbox.present();
+    }
+
 }
