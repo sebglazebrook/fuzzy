56d682823e3e09ae49982f2312ff2278ae5c60fb
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-09-19 17:09:32 +1000

    Handle killing thread in FileFinder on desctruction

--- src/fuzzy/file_finder.rs
+++ src/fuzzy/file_finder.rs
@@ -77,7 +77,7 @@ impl FileFinder {
 impl Drop for FileFinder {
 
     fn drop(&mut self) {
-        let _ = self.tx.send(1);
-        thread::sleep_ms(1);
+        self.tx.send(1).unwrap();
+        thread::sleep_ms(1); // waiting for the nested thread in FileFinder to be killed.
     }
 }
