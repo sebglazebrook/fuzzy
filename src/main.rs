f8dc7250ddcee4a8470376b6c48aaa2975983264
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-10-15 21:45:09 +1100

    testing docopt crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 extern crate fuzzy;
+extern crate docopt;
+extern crate rustc_serialize;
 
 use fuzzy::initialize;
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Fuzzy: the fuzzy file finder.
+
+Usage:
+  fuzzy [FLAGS]
+
+Flags:
+  -h --help     Show this screen. 
+  --auto-copy   Automatically copy result to clipboard.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_auto_copy: bool,
+}
+
 
 fn main() {
+    let args: Args = Docopt::new(USAGE)
+                             .and_then(|d| d.decode())
+                             .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
     initialize();
 }
